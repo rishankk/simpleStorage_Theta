3cb6e2eaa187c9693809d13383707a67f61823bd
rishankk <rishankkumarkatiyar@gmail.com>
2022-01-30 13:06:08 +0530

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,26 @@
+from brownie import accounts, config, SimpleStorage, network
+
+
+def deploy_simple_storage():
+    # account = accounts.load("codeAccount")
+    # account = accounts.add(config["wallets"]["from_key"])
+    # account = accounts[0]
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    uodated_stored_value = simple_storage.retrieve()
+    print(uodated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    deploy_simple_storage()
